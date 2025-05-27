7787fe03e3c8fc35cfddac2a7a2b194770dc9848
Martin Simonyan <martinsimonyan2563@gmail.com>
2025-05-27 22:58:54 +0400

    Update inquisitor.py
    Updated to new version 0.1.5!

--- inquisitor.py
+++ inquisitor.py
@@ -10,12 +10,20 @@ from tools import scanner
 
 ascii_banner = pyfiglet.figlet_format("INQUISITOR")
 print(ascii_banner)
+print("Version 0.1.5")
 print("-" * 50)
 
 scanner_instance = scanner.PortScanner()
 
-try:
-    scanner_instance.scan_port()
 
-except KeyboardInterrupt:
-    print("Shutting down")
+
+def tool():
+    mode = input("Pick the tool you wanna use: \n 1. Port Scanner\n")
+    if mode == 1 or "Port Scanner":
+        try:
+            scanner_instance.scan_port()
+
+        except KeyboardInterrupt:
+            print("Shutting down")
+
+tool()
