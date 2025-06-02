f3142a571776e40194894ec585f97bf0f8160fb0
Martin Simonyan <martinsimonyan2563@gmail.com>
2025-06-02 14:58:36 +0400

    Misspell fix inquisitor.py

--- inquisitor.py
+++ inquisitor.py
@@ -5,7 +5,7 @@ from colorama import Fore, Style
 print(f"{Fore.BLUE}-" * 50)
 ascii_banner = pyfiglet.figlet_format(f"INQUISITOR")
 print(ascii_banner)
-print("Version 0.2.0 ALPHA")
+print("Version 0.2.0 BETA")
 print("-" * 50)
 
 scanner_instance = scanner.PortScanner()
