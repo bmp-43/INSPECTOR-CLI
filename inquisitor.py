ea62c9a652ff0a1e80ded8f03d067fd64cd04523
Martin Simonyan <martinsimonyan2563@gmail.com>
2025-06-02 14:35:16 +0400

    New version of inquisitor.py!
    New version 0.2.0 Alpha Subdomain enumeration tool was added!

--- inquisitor.py
+++ inquisitor.py
@@ -1,29 +1,32 @@
 import pyfiglet
-import sys
-import socket
-from datetime import datetime
-import re
-import os
-import concurrent.futures
-from tools import scanner
-
-
-ascii_banner = pyfiglet.figlet_format("INQUISITOR")
+from tools.scanner import scanner
+from tools.enumerator import enumerator
+from colorama import Fore, Style
+print(f"{Fore.BLUE}-" * 50)
+ascii_banner = pyfiglet.figlet_format(f"INQUISITOR")
 print(ascii_banner)
-print("Version 0.1.5")
+print("Version 0.2.0 ALPHA")
 print("-" * 50)
 
 scanner_instance = scanner.PortScanner()
 
 
 
-def tool():
-    mode = input("Pick the tool you wanna use: \n 1. Port Scanner\n")
-    if mode == 1 or "Port Scanner":
+def weapon():
+    mode = input(f"{Style.RESET_ALL}Pick the tool you wanna use: \n 1. Port Scanner\n 2. Enumerator\n")
+    if mode == "1" or mode.lower == "port scanner":
         try:
             scanner_instance.scan_port()
 
         except KeyboardInterrupt:
             print("Shutting down")
+    elif mode == "2" or mode.lower == "enumerator":
+        try:
+            enumerator.tool()
 
-tool()
+        except KeyboardInterrupt:
+            print("Shutting down")        
+try:
+    weapon()
+except KeyboardInterrupt:
+    print("Shutting Down...")
