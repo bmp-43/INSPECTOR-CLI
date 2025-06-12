1cd5ef2a0c9883037fcf72aa645b1bfe3e1952c4
Martin Simonyan <martinsimonyan2563@gmail.com>
2025-06-12 21:06:06 +0400

    New version for inquisitor!

--- inquisitor.py
+++ inquisitor.py
@@ -5,7 +5,7 @@ from colorama import Fore, Style
 print(f"{Fore.BLUE}-" * 50)
 ascii_banner = pyfiglet.figlet_format(f"INQUISITOR")
 print(ascii_banner)
-print("Version 0.2.2 BETA")
+print("Version 0.2.3 BETA")
 print("-" * 50)
 
 scanner_instance = scanner.PortScanner()
