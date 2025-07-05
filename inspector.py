96b1e1f9740d57ae9a0be163a270aa7717e9fdc5
Martin Simonyan <martinsimonyan2563@gmail.com>
2025-07-05 18:22:12 +0400

    Update inspector.py

--- inspector.py
+++ inspector.py
@@ -2,18 +2,18 @@ import pyfiglet
 from tools.scanner import scanner
 from tools.enumerator import enumerator
 from colorama import Fore, Style
-from tools.hashscan import hashscan
+from tools.analyser import analyser
 print(f"{Fore.BLUE}-" * 50)
 ascii_banner = pyfiglet.figlet_format(f"INSPECTOR")
 print(ascii_banner)
-print("Version 0.3.1 BETA")
+print("Version 0.3.2 BETA")
 print("-" * 50)
 
 scanner_instance = scanner.PortScanner()
 
 
 def weapon():
-    mode = input(f"{Style.RESET_ALL}Pick the tool you wanna use: \n 1. Port Scanner\n 2. Enumerator\n 3. Hash Scanner \n")
+    mode = input(f"{Style.RESET_ALL}Pick the tool you wanna use: \n 1. Port Scanner\n 2. Enumerator\n 3. Malware Analyser \n")
     if mode == "1" or mode.lower() == "port scanner":
         try:
             scanner_instance.scan_port()
@@ -24,9 +24,10 @@ def weapon():
             enumerator.tool()
         except KeyboardInterrupt:
             print("Shutting down")        
-    elif mode == "3" or mode.lower() == "hash scanner":
+    elif mode == "3" or mode.lower() == "Malware Analyser":
         try:
-            hashscan.start()
+            print(f"{Fore.CYAN}[i]{Style.RESET_ALL} {Fore.LIGHTBLACK_EX}Note that Malware analyser uses VirusTotal API. Check the config.txt{Style.RESET_ALL}")
+            analyser.main()
         except KeyboardInterrupt:
             print("Shutting down") 
 
@@ -35,6 +36,7 @@ try:
         weapon()
 except KeyboardInterrupt:
     print("Shutting Down...")
+
 
 
 
